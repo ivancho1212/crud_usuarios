0d5cff4a76aad61d2da84414d552f244fbab6551
ivancho1212 <helpme.bog.col@gmail.com>
2023-08-11 14:09:11 -0500

    Initial commit

---
+++ views/products/edit.php
@@ -0,0 +1,35 @@
+<!--content -->
+<div class="content">
+    <div class="container-fluid">
+        <br>
+        <div class="row">
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-body">
+                        <a href="?c=products&m=index" class="btn btn-link">← Cancelar o volver</a>
+                          <form action="">
+                            <div class="form-group mt-3">
+                                <label for="">Nombre</label>
+                                <input type="text" class="form-control" value="CocaCola">
+                            </div>
+                            <div class="form-group mt-3">
+                                <label for="">Precio</label>
+                                <input type="text" class="form-control" value="$5.000">
+                            </div>
+                            <div class="form-group mt-3">
+                                <label for="">Descripcion</label>
+                                <input type="text" class="form-control" value="TAMAÑO FAMILIAR 3 LITROS">
+                            </div>
+                            <div class="form-group mt-3">
+                                <label for="">Logo</label>
+                                <input type="file" class="form-control">
+                            </div>
+                            <button class="btn btn-success w-100 mt-4">Actualizar compañia</button>
+                          </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end content -->
